048eeece51b0ff7033b1b37ca962b4efbe96fb24
haosome6 <zhzjim@gmail.com>
2020-05-03 18:53:28 -0400

    Finish Card comparison with test file

--- Card.py
+++ Card.py
@@ -1,23 +1,75 @@
+from __future__ import annotations
+
+compare_map = {
+    'A': 14,
+    'K': 13,
+    'Q': 12,
+    'J': 11,
+    '10': 10,
+    '9': 9,
+    '8': 8,
+    '7': 7,
+    '6': 6,
+    '5': 5,
+    '4': 4,
+    '3': 3,
+    '2': 2
+}
+
+
 class Card:
     """A card.
 
     === Private Attributes ===
-    number:
+    _number:
         The number of the card, "A" is the biggest, and "2" is the smallest.
-    suit:
+    _suit:
         The suit of the card.
+
+    === Representation Invariants ===
+    - _suit must be one of "s", "h", "d", and "c"(represents spade, heart,
+      diamond, and club respectively)
     """
     _number: str
     _suit: str
 
-    def __init__(self, number: int, suit: str) -> None:
+    def __init__(self, number: str, suit: str) -> None:
         """Initialize the card with number <number> and suit <suit>.
         """
+        self._number = number
+        self._suit = suit
+
+    def __eq__(self, other: Card) -> bool:
+        """Return True iff this Card is equal to <other>.
+
+        Two cards are equal if they have the same number.
+        """
+        return compare_map[self._number] == compare_map[other._number]
+
+    def __ne__(self, other: Card) -> bool:
+        """Return True iff this Card is not equal to <other>.
+        """
+        return not self.__eq__(other)
 
     def __lt__(self, other: Card) -> bool:
-        """Compare this card with another card <other> by number, "A" is the
-        biggest.
+        """Return True iff this Card is less than <other>.
         """
+        return compare_map[self._number] < compare_map[other._number]
+
+    def __le__(self, other: Card) -> bool:
+        """Return True iff this Card is less than or equal to <other>.
+        """
+        return compare_map[self._number] <= compare_map[other._number]
+
+    def __gt__(self, other: Card) -> bool:
+        """Return True iff this Card is greater than <other>.
+        """
+        return not self.__le__(other)
+
+    def __ge__(self, other: Card) -> bool:
+        """Return True iff this Card is greater than or equal to <other>.
+        """
+        return not self.__lt__(other)
 
     def get_number(self) -> str:
         """Return the number of the card.
---
+++ test_card.py
@@ -0,0 +1,21 @@
+from Card import Card
+
+
+def test__ls_() -> None:
+    c1 = Card('A', 's')
+    c2 = Card('J', 's')
+    c3 = Card('5', 's')
+    c4 = Card('5', 'c')
+    assert c1 > c2
+    assert c2 > c3
+    assert c3 == c4
+    assert c3 >= c4
+    assert c2 >= c4
+    assert c4 < c2
+    assert c4 <= c3
+
+
+if __name__ == "__main__":
+    import pytest
+
+    pytest.main(['test_card.py'])
