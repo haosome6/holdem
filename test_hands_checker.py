0cd04ee9845494f1df54a9fd0b9a996cf9bd67e6
lockie0228 <lawrence0228@outlook.com>
2020-05-15 17:28:49 -0400

    add test_straight method in test_hands_checker

--- test_hands_checker.py
+++ test_hands_checker.py
@@ -39,9 +39,10 @@ def test_four_of_a_kind() -> None:
     c9d = Card('9', 'd')
     cAd = Card('A', 'd')
     c5s = Card('5', 's')
+    c8d = Card('8', 'd')
 
     # four of a kind of 9, with kicker A
-    cards_1 = [cAd, c9s, c9h, c9c, c9d, c5s]
+    cards_1 = [cAd, c9s, c9h, c8d, c9c, c9d, c5s]
     assert hands_checker.get_hands_type(cards_1) == 'four of a kind'
     assert hands_checker.get_hands_cards(cards_1)[0].number == '9'
     assert hands_checker.get_hands_cards(cards_1)[4].number == 'A'
@@ -66,21 +67,70 @@ def test_flush() -> None:
     assert result_cards[-1].number == '3'
 
 
-def test_full_house() -> None:
+def test_full_house1() -> None:
     c9s = Card('9', 's')
     c9h = Card('9', 'h')
     c9c = Card('9', 'c')
     cAd = Card('A', 'd')
     cAs = Card('A', 's')
     cAc = Card('A', 'c')
+    c8d = Card('8', 'd')
 
-    # four of a kind of 9, with kicker A
-    cards_1 = [cAd, c9h, cAs, c9s, c9c, cAc]
+    cards_1 = [cAd, c9h, cAs, c8d, c9s, c9c, cAc]
     assert hands_checker.get_hands_type(cards_1) == 'full house'
     assert len(hands_checker.get_hands_cards(cards_1)) == 5
     assert hands_checker.get_hands_cards(cards_1)[0].number == 'A'
     assert hands_checker.get_hands_cards(cards_1)[3].number == '9'
     assert hands_checker.get_hands_cards(cards_1)[-1].number == '9'
+
+
+def test_full_house2() -> None:
+    c9s = Card('9', 's')
+    c9h = Card('9', 'h')
+    c9c = Card('9', 'c')
+    cAd = Card('A', 'd')
+    cAs = Card('A', 's')
+    c5c = Card('5', 'c')
+    c8d = Card('8', 'd')
+
+    cards_1 = [cAd, c9h, cAs, c8d, c9s, c9c, c5c]
+    assert hands_checker.get_hands_type(cards_1) == 'full house'
+    assert len(hands_checker.get_hands_cards(cards_1)) == 5
+    assert hands_checker.get_hands_cards(cards_1)[0].number == '9'
+    assert hands_checker.get_hands_cards(cards_1)[3].number == 'A'
+    assert hands_checker.get_hands_cards(cards_1)[-1].number == 'A'
+
+
+def test_straight1() -> None:
+    c5s = Card('5', 's')
+    c6h = Card('6', 'h')
+    c7c = Card('7', 'c')
+    c8d = Card('8', 'd')
+    c9s = Card('9', 's')
+    cAc = Card('A', 'c')
+    c3d = Card('3', 'd')
+
+    cards_1 = [c9s, c3d, c5s, c7c, cAc, c6h, c8d]
+    assert hands_checker.get_hands_type(cards_1) == 'straight'
+    assert len(hands_checker.get_hands_cards(cards_1)) == 5
+    assert hands_checker.get_hands_cards(cards_1)[0].number == '9'
+    assert hands_checker.get_hands_cards(cards_1)[-1].number == '5'
+
+
+def test_straight2() -> None:
+    cAs = Card('A', 's')
+    c2h = Card('2', 'h')
+    c3c = Card('3', 'c')
+    c4d = Card('4', 'd')
+    c5s = Card('5', 's')
+    cQc = Card('Q', 'c')
+    cJd = Card('J', 'd')
+
+    cards_1 = [cJd, c3c, cQc, c5s, cAs, c2h, c4d]
+    assert hands_checker.get_hands_type(cards_1) == 'straight'
+    assert len(hands_checker.get_hands_cards(cards_1)) == 5
+    assert hands_checker.get_hands_cards(cards_1)[0].number == '5'
+    assert hands_checker.get_hands_cards(cards_1)[-1].number == 'A'
 
 
 if __name__ == "__main__":
