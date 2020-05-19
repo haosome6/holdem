608332fb2d92c59cdc75332f14faa127f8866cdc
Haozhe <zhzjim@gmail.com>
2020-05-19 18:17:53 -0400

    finish player class

--- player.py
+++ player.py
@@ -16,18 +16,28 @@ class Player:
     _password: str
     _total_chips: int
 
-
-    def __init__(self, username: str, password: str, total_chips: int) -> None:
+    def __init__(self, username: str, password: str) -> None:
         """Initialize the player with username <username>, password <password>
         and total_chips <total_chips>.
         """
+        self._username = username
+        self._password = password
+        self._total_chips = 0
 
     def log_in(self, password: str) -> bool:
         """Return True iff <password> matches this Player's password.
         """
+        return self._password == password
 
     def get_username(self) -> str:
         """Return the username of this Player.
         """
+        return self._username
 
-    def cash_out(self) -> None:
+    def add_to_total_chips(self, amount: int) -> None:
+        """Deposit money into _total_chips."""
+        self._total_chips += amount
+
+    def take_from_total_chips(self, amount: int) -> None:
+        """Take money from _total_chips into gaming."""
+        self._total_chips -= amount
