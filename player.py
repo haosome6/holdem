446677452045622ffb2c6e1cd89c167cf4a096b2
Haozhe <zhzjim@gmail.com>
2020-08-07 20:33:51 +0800

    clean attributes

--- player.py
+++ player.py
@@ -12,32 +12,19 @@ class Player:
     total_chips:
         The total chips of the player that is not playing.
     """
-    _username: str
+    username: str
     _password: str
-    _total_chips: int
+    total_amount: int
 
     def __init__(self, username: str, password: str) -> None:
         """Initialize the player with username <username>, password <password>
         and total_chips <total_chips>.
         """
-        self._username = username
+        self.username = username
         self._password = password
-        self._total_chips = 0
+        self.total_amount = 0
 
     def log_in(self, password: str) -> bool:
         """Return True iff <password> matches this Player's password.
         """
         return self._password == password
-
-    def get_username(self) -> str:
-        """Return the username of this Player.
-        """
-        return self._username
-
-    def add_to_total_chips(self, amount: int) -> None:
-        """Deposit money into _total_chips."""
-        self._total_chips += amount
-
-    def take_from_total_chips(self, amount: int) -> None:
-        """Take money from _total_chips into gaming."""
-        self._total_chips -= amount
